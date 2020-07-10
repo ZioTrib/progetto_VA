8cbf635d2b71eb849af501df7cef218c0076b412
Daniele Tribuzio <d.tribuzio@studenti.unipi.it>
2020-07-10 15:52:09 +0200

    introducing crossfilter and make form list linked to data

--- applicazione/src/components/dashboard.vue
+++ applicazione/src/components/dashboard.vue
@@ -1,40 +1,44 @@
 <template>
   <b-container class="dashboard">
-    <b-row>
+    <b-row class="text-center">
       <b-col cols="9">
-        <h5>Mappa dei pozzi in italia</h5>
+        <h2>Mappa dei pozzi in italia</h2>
         <div style="height:500px; background-color: beige"></div>
       </b-col>
       <b-col cols="3">
-        <h5>informazioni del pozzo</h5>
+        <h2>informazioni del pozzo</h2>
         <div style="height:250px; background-color: beige"></div>
         <b-col>
           <div>
-            <h5>Attributi</h5>
+            <h4>Attributi</h4>
+            <b-form-select v-model="uso.selected" :options="uso.options"></b-form-select>
+            <div class="mt-3">tipo di utilizzo: <strong>{{ selected }}</strong></div>
             <b-form-select v-model="selected" :options="options"></b-form-select>
-            <div class="mt-3">Selected: <strong>{{ selected }}</strong></div>
+            <div class="mt-3">tipo di utilizzo: <strong>{{ selected }}</strong></div>
+            <b-form-select v-model="selected" :options="options"></b-form-select>
+            <div class="mt-3">tipo di utilizzo: <strong>{{ selected }}</strong></div>
           </div>
         </b-col>
       </b-col>
     </b-row>
 
-    <b-row>
+    <b-row class="text-center">
       <b-col>
-        <h5> quota e profondità pozzi </h5>
+        <h3> quota e profondità pozzi </h3>
         <div style="height:400px; background-color: beige"></div>
       </b-col>
     </b-row>
-    <b-row>
+    <b-row class="text-center">
       <b-col>
-        <h5> 1</h5>
+        <h3> 1</h3>
         <div style="height:250px; background-color: beige"></div>
       </b-col>
       <b-col>
-        <h5> 2 </h5>
+        <h3> 2 </h3>
         <div style="height:250px; background-color: beige"></div>
       </b-col>
       <b-col>
-        <h5> 3 </h5>
+        <h3> 3 </h3>
         <div style="height:250px; background-color: beige"></div>
       </b-col>
     </b-row>
@@ -43,6 +47,14 @@
 
 
 <script>
+import crossfilter from 'crossfilter';
+
+// crossfilter data management
+let cf; // crossfilter instance
+let dnome;
+let dprof;
+let duso;
+// let dVesselType;
 
 
 export default {
@@ -51,15 +63,11 @@ export default {
   },
   data() {
     return {
-      year: {
-        value: 2001,
-        options: [2001, 2002, 2003],
+      uso: {
+        selected: '1',
+        options: ['1', '2', '3'],
+        reports: [],
       },
-      boatType: {
-        value: 'All',
-        options: ['All', 'Rustic', 'Raft'],
-      },
-      reports: [],
     };
   },
 
@@ -68,11 +76,25 @@ export default {
       .then(res => res.json())
       .then((out) => {
         this.reports = out.map(d => ({
+          uso: d.uso,
           prof: +d.prof,
-          key: +d.key,
+          nome: d.nome,
         }));
+        cf = crossfilter(this.reports);
+        duso = cf.dimension(d => d.uso);
+        dnome = cf.dimension(d => d.nome);
+        dprof = cf.dimension(d => d.prof);
+        duso.filter('AGROZOOTECNICO');
+        /* eslint-disable */
+        console.log('uso agro', duso.group().reduceCount().all());
+        console.log('key', dnome.group().reduceCount().all());
+        console.log('recordType', dprof.group().reduceCount().all());
+        /* eslint-disable */
+        this.uso.options = duso.group().reduceCount().all().map(v => v.key);
+        this.uso.value = this.uso.options[0];
       });
   },
+
 };
 </script>
 
