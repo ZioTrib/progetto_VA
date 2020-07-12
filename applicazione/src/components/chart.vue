35492e74278db266f1abfe1a27615564fdff4f6a
Daniele Tribuzio <d.tribuzio@studenti.unipi.it>
2020-07-12 19:03:12 +0200

    bar chart added

---
+++ applicazione/src/components/chart.vue
@@ -0,0 +1,43 @@
+<template>
+    <vue-plotly :data="data" :layout="layout" :options="options"/>
+</template>
+
+<script>
+import VuePlotly from '@statnett/vue-plotly';
+
+export default {
+  name: 'chart',
+  components: {
+    VuePlotly,
+  },
+  props: {
+    cfAggregation: Array,
+  },
+  data() {
+    return {
+      data: [{
+        type: 'bar',
+        x: [1, 3],
+        y: [2, 4],
+      }],
+      layout: {
+        xaxis: {
+          type: 'category',
+        },
+      },
+      options: {
+      },
+    };
+  },
+  watch: {
+    cfAggregation(datum) {
+      this.data[0].y = datum.map(d => d.value * -1);
+      this.data[0].x = datum.map(d => d.key);
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
--- applicazione/src/components/dashboard.vue
+++ applicazione/src/components/dashboard.vue
@@ -24,24 +24,8 @@
               </b-form-group>
             </div>
             <div>
-              <b-form-group label="Stacked (vertical) switch style checkboxes" class="text-left">
-                <b-form-checkbox-group
-                  v-model="uso.selected"
-                  :options="uso.options"
-                  switches
-                  stacked
-                ></b-form-checkbox-group>
-              </b-form-group>
             </div>
             <div>
-              <b-form-group label="Stacked (vertical) switch style checkboxes" class="text-left">
-                <b-form-checkbox-group
-                  v-model="uso.selected"
-                  :options="uso.options"
-                  switches
-                  stacked
-                ></b-form-checkbox-group>
-              </b-form-group>
             </div>
           </div>
         </b-col>
@@ -51,7 +35,9 @@
     <b-row class="text-center">
       <b-col>
         <h3> quota e profondità pozzi </h3>
-        <div style="height:400px; background-color: beige"></div>
+        <div style="height:600px; background-color: beige">
+        <chart :cfAggregation="pozzi"></chart>
+        </div>
       </b-col>
     </b-row>
     <b-row class="text-center">
@@ -75,19 +61,18 @@
 <script>
 import crossfilter from 'crossfilter';
 import informazioni from './informazioni';
+import chart from './chart';
 
 // crossfilter data management
 let cf; // crossfilter instance
-// let dnome;
-// let dprof;
 let duso;
-// let dVesselType;
 
 
 export default {
   name: 'dashboard',
   components: {
     informazioni,
+    chart,
   },
   data() {
     return {
@@ -96,6 +81,8 @@ export default {
         options: ['TUTTI'],
       },
       numRecords: 0,
+      reports: [],
+      pozzi: [],
     };
   },
 
@@ -103,28 +90,29 @@ export default {
     fetch('static/data/pozzi.json')
       .then(res => res.json())
       .then((out) => {
-        const reports = out.map(d => ({
+        this.reports = out.map(d => ({
           uso: d.uso,
-
           prof: +d.prof,
           nome: d.nome,
         }));
 
-        cf = crossfilter(reports);
-        // duso = cf.dimension(d => d.uso);
+        cf = crossfilter(this.reports);
         duso = cf.dimension(d => d.uso);
-        // dprof = cf.dimension(d => d.prof);
 
         cf.groupAll().reduceCount().value();
-
         this.uso.options = ['TUTTI'].concat(duso.group().reduceCount().all().map(v => v.key));
         this.uso.selected = this.uso.options[0];
         this.refreshCounters();
+        this.refreshCharts();
       });
   },
   methods: {
     refreshCounters() {
       this.numRecords = cf.groupAll().reduceCount().value();
+    },
+    refreshCharts() {
+      /* eslint-disable */
+      this.pozzi = this.reports.map(v => ({ key: v.nome, value: +v.prof }));
     },
   },
   watch: {
@@ -136,6 +124,7 @@ export default {
           duso.filter(newVal.selected);
         }
         this.refreshCounters();
+        this.refreshCharts();
       },
       deep: true, // force watching within properties
     },
